f9f834d26e9aee9eaa978a154111ba62c4861eb5
AhmedAlosif <ahmed.k.alosif@gmail.com>
2025-07-02 12:51:24 +0300

    Create ortools.py

---
+++ MVRP_Engine/solvers/ortools.py
@@ -0,0 +1,40 @@
+def solve_vrp():
+    data = create_vrp_data_model()
+
+    manager = pywrapcp.RoutingIndexManager(
+        len(data["distance_matrix"]),
+        data["num_vehicles"],
+        data["depot"]
+    )
+
+    routing = pywrapcp.RoutingModel(manager)
+
+    def distance_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data["distance_matrix"][from_node][to_node]
+
+    routing.SetArcCostEvaluatorOfAllVehicles(distance_callback)
+
+    # Add demand constraint
+    def demand_callback(from_index):
+        from_node = manager.IndexToNode(from_index)
+        return data["demands"][from_node]
+
+    demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0,
+        data["vehicle_capacities"],
+        True,
+        "Capacity"
+    )
+
+    # Setting first solution strategy
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    solution = routing.SolveWithParameters(search_parameters)
+  
+solve_vrp()
